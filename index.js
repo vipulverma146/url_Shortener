4bdb19af163d1cad6f9d43d851efb577771b75df
vipulverma146 <47173395+vipulverma146@users.noreply.github.com>
2022-02-21 00:08:25 +0530

    finalcode Done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const ShortUrl = require('./models/shortUrl');
 const app = express()
-const port=5050;
+const port=process.env.PORT||5050;
 
 mongoose.connect('mongodb://localhost/urlShortener', {
   useNewUrlParser: true, useUnifiedTopology: true
